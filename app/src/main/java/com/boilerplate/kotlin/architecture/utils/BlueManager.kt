64f39f87a965e3b641b718581313311f287c93a5
a.belichenko <a.belichenko@gmail.com>
2018-02-02 11:41:07 +0200

    broadcast receiver to rx

--- app/src/main/java/com/boilerplate/kotlin/architecture/utils/BlueManager.kt
+++ app/src/main/java/com/boilerplate/kotlin/architecture/utils/BlueManager.kt
@@ -1,16 +1,17 @@
 package com.boilerplate.kotlin.architecture.utils
 
-import android.content.Context
-import rx.Observable
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import com.boilerplate.kotlin.architecture.models.BlueDevice
-import java.lang.IllegalStateException
-import android.content.Intent
 import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
+import com.boilerplate.kotlin.architecture.models.BlueDevice
+import rx.Observable
+import rx.Subscriber
+import rx.subscriptions.Subscriptions
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
+import java.lang.IllegalStateException
 
 
 /**
@@ -18,42 +19,42 @@ import java.util.concurrent.TimeUnit
  */
 class BlueManager(private val context: Context) {
 
-    private val deviceList : ArrayList<BlueDevice> = ArrayList()
-    private val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-            if (BluetoothDevice.ACTION_FOUND == action) {
-                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-                Timber.d("New blue device")
-                deviceList.add(BlueDevice(device.name, device.address))
-            }
-        }
-    }
-
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-    fun getDeviceList() : Observable<List<BlueDevice>>{
-        return if (bluetoothAdapter == null){
+    fun getBlueDevices(): Observable<BlueDevice> {
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+
+        return if (bluetoothAdapter == null) {
             Observable.error(IllegalStateException())
-        }else if(!bluetoothAdapter.isEnabled){
+        } else if (!bluetoothAdapter.isEnabled) {
             Observable.error(IllegalAccessException())
-        }else{
-            Observable.just(deviceList.toList())
-                    .doOnSubscribe({
-                        Timber.d("Start blue receiver")
-                        bluetoothAdapter.startDiscovery()
-                        context.registerReceiver(receiver, filter) })
-                    .zipWith(Observable.interval(19, TimeUnit.SECONDS), { t1, _ -> t1 })
-                    .doOnNext({
+        } else {
+            Observable.create(object : Observable.OnSubscribe<BlueDevice> {
+                override fun call(subscriber: Subscriber<in BlueDevice>) {
+                    val receiver = object : BroadcastReceiver() {
+                        override fun onReceive(context: Context, intent: Intent) {
+                            val action = intent.action
+                            if (BluetoothDevice.ACTION_FOUND == action) {
+                                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                                Timber.d("New blue device")
+                                subscriber.onNext(BlueDevice(device.name?: "", device.address))
+                            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
+                                subscriber.onCompleted()
+                            }
+                        }
+                    }
+                    Timber.d("Start blue receiver")
+                    bluetoothAdapter.startDiscovery()
+                    context.registerReceiver(receiver, filter)
+
+                    subscriber.add(Subscriptions.create {
                         Timber.d("Stop blue receiver")
                         bluetoothAdapter.cancelDiscovery()
-                        context.unregisterReceiver(receiver) })
+                        context.unregisterReceiver(receiver)
+                    })
+                }
+            })
         }
-    }
-
-    private fun checkBlueState() : Observable<String>{
-        return Observable.error(IllegalStateException())
     }
 }
